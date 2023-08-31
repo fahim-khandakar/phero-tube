6cd11a6b68ec1fe931cf5f45b46ab772095de6b0
fahim-khandakar <fahimkhandakar01@gmail.com>
2023-09-01 01:20:18 +0600

    card all ok

--- script.js
+++ script.js
@@ -30,8 +30,8 @@ const categories = (categoryData) => {
       "text-xs",
       "md:text-base"
     );
-    button.innerText = `${category.category}`;
-    button.setAttribute("onclick", `videoDataLoad(${category.category_id})`);
+    button.innerText = `${category?.category}`;
+    button.setAttribute("onclick", `videoDataLoad(${category?.category_id})`);
     categoryContainer.appendChild(button);
   });
 };
@@ -43,5 +43,76 @@ const videoDataLoad = async (categoryId) => {
   );
   const data = await res.json();
   const videoData = data.data;
-  console.log(videoData);
+
+  const videoContainer = document.getElementById("video-container");
+  const throwError = document.getElementById("throw-error");
+  throwError.innerText = "";
+  videoContainer.textContent = "";
+
+  //   error throw
+  if (!data.status === true) {
+    const div = document.createElement("div");
+    div.classList.add("w-full", "mx-auto", "mt-20");
+    div.innerHTML = `
+    <img class = "mx-auto" src = "icon.png" />
+    <p class = "text-center text-xl font-semibold md:text-3xl md:font-bold mt-5">Oops!! Sorry, There is no <br/> content here</p>
+    `;
+    throwError.appendChild(div);
+  }
+
+  videoData.forEach((video) => {
+    console.log(video);
+    const div = document.createElement("div");
+    const time = +video.others.posted_date;
+    const fullMinutes = time / 60;
+    const hours = parseInt(fullMinutes / 60);
+    const minutes = parseInt(fullMinutes % 60);
+
+    // console.log(hours, minutes);
+    div.classList.add("card", "bg-base-100", "shadow-xl");
+    div.innerHTML = `
+        <figure>
+        <img class = "h-52 rounded-lg relative"
+          src=${video?.thumbnail}
+          alt="Shoes"
+        />
+        ${
+          time
+            ? `<p class = "absolute right-3 top-40 bg-black text-white px-2 py-1 rounded-lg">${hours}hrs ${minutes}min ago <p/>`
+            : ""
+        }
+      </figure>
+      <div class="card-body px-3">
+      <div class="flex gap-3">
+      <!-- first  -->
+      <div>
+        <img
+        class = "w-10 h-10 rounded-full object-cover" 
+        src=${video?.authors[0]?.profile_picture}
+        alt="" />
+      </div>
+      <!-- second  -->
+      <div>
+        <h3 class = "text-xl font-bold">${video?.title}</h3>
+        <div class = "flex items-center mt-3">
+        <p class = "font-semibold">${video?.authors[0]?.profile_name}</p>
+        <p class = "font-semibold">${
+          video?.authors[0]?.verified
+            ? `<image class = "w-16 h-10" src= "badge2.png" />`
+            : ""
+        }</p>
+        </div>
+        <p class = "pt-2 font-semibold">${video?.others?.views}</p>
+    </div>
+      </div>
+
+        `;
+    videoContainer.appendChild(div);
+  });
+  //   console.log(videoData);
+};
+videoDataLoad("1000");
+
+const sortVideo = () => {
+  console.log("hello");
 };
